bbe0437fd774e8dd892c9d78ad26893b2337c6fd
hakugakusha <87151999+hakugakusha@users.noreply.github.com>
2021-09-18 14:12:00 -0700

    Update Lyric_scraper.py

--- Lyric_scraper.py
+++ Lyric_scraper.py
@@ -1,10 +1,15 @@
 from lyricsgenius import Genius
 
-genius = Genius('4Q-mvPPNijWi36HAGc3sIOyNGxWfjNaxiR68XK54mei2KjtRTIAHP7HzM_tEIpB2', skip_non_songs = True, remove_section_headers = True)
+genius = Genius('4Q-mvPPNijWi36HAGc3sIOyNGxWfjNaxiR68XK54mei2KjtRTIAHP7HzM_tEIpB2', skip_non_songs=True)
 file = open("C:\\Users\\Brooklyn\\PycharmProjects\\Learning_Python\\Python_Projects\\lyrics.txt", "w")
 
 artist_name = input("What artist do you want? ")
 song_title = input("What song are you looking for? ")
 
-song = genius.search_song(song_title, artist_name)
-file.write(song.lyrics + "\nEnd of Lyrics")
+
+def scrape_lyrics(artist, title):
+    songs = (genius.search_song(song_title, artist_name))
+    list = [songs.lyrics[:-29]]
+    file.write("\n \nEnd of Lyrics".join(list))
+
+scrape_lyrics(artist_name, song_title)
